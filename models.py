21101e8b17a8dba7d42822579a6ceaa7cc4ea843
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-11 13:58:18 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,10 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from database import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
+class Music(Base):
+    __tablename__ = "music"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password_hash = Column(String)
+    title = Column(String, index=True)
+    artist = Column(String, index=True)
+    filename = Column(String)
