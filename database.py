825ba3f13fbacc2bf87be959368048ab88edd2d0
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-11 13:57:27 +0200

    Create database.py

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "music.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def init_db():
+    from models import Music
+    Base.metadata.create_all(bind=engine)
