26a8c8905c8b91ce6d9fe010685b64d349d4d32c
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-13 17:51:45 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class User(Base):
-    __tablename__ = "users"
-    id       = Column(Integer, primary_key=True, index=True)
-    email    = Column(String, unique=True, index=True)
-    password = Column(String)
-
 class Track(Base):
     __tablename__ = "tracks"
-    id       = Column(Integer, primary_key=True, index=True)
-    title    = Column(String)
-    artist   = Column(String)
-    genre    = Column(String)
-    album    = Column(String)
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    artist = Column(String)
+    album = Column(String)
+    genre = Column(String)
     filename = Column(String)
-    cover    = Column(String)
+    cover_url = Column(String)
+    user_id = Column(String)
