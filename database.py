a592bef3d1168d5635c2b0fad17332418967afbe
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-13 15:12:53 +0200

    Update database.py

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DB_URL = "sqlite:///./music.db"
+DB_URL = os.getenv("DATABASE_URL", "sqlite:///./music.db")
 engine  = create_engine(DB_URL, connect_args={"check_same_thread": False})
 Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base    = declarative_base()
 
 def init_db():
-    from models import User, Track   # noqa
+    from models import User, Track          # noqa
     Base.metadata.create_all(bind=engine)
