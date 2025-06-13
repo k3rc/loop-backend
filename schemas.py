32b6b7e768faf173db1ef9a9df9d190673c5dfac
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-13 14:25:50 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
 
-class MusicCreate(BaseModel):
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type:   str
+
+class TrackOut(BaseModel):
+    id: int
     title: str
     artist: str
     filename: str
+    class Config:
+        orm_mode = True
