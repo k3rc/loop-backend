63877069db23eee7d6cb251fadc7676cc2282129
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-15 20:13:44 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import Session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb.db"
+DATABASE_URL = "sqlite:///./tracks.db"  # Или ENV-переменная на проде
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
